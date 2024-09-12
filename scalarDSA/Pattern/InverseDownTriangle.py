87fc761d5778ccc37b342b10e1c6ba56f895a53c
Shikher Jain <shikherjain786@gmail.com>
2024-09-13 02:38:58 +0530

    SCALAR

--- scalarDSA/Pattern/InverseDownTriangle.py
+++ scalarDSA/Pattern/InverseDownTriangle.py
@@ -11,3 +11,18 @@ for i in range(1,R+1):                          #|*                   *
     for l in range(i):                                         
         print("*",end=" ")                                      
     print()                                                  
+
+
+
+# R=int(input("Enter how many Rows you wants to print: "))        
+# for i in range(1,R+1):                          #|*                   * 
+#     for j in range(i):                          #|* *               * * 
+#         print("*",end="")                       #|* * *           * * *    R=6
+#     # for k in range(R-i):                      #|* * * *       * * * * 
+#     for k in range(2*(R-i)):                    #|* * * * *   * * * * * 
+#         print(" ",end="")                       #|* * * * * * * * * * * 
+#     # for k in range(R-i):                                    
+#         # print(" ",end=" ")                                    
+#     for l in range(i):                                         
+#         print("*",end="")                                      
+#     print()                                                  
