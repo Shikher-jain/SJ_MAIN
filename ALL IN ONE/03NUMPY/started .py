d307304bfdad80f9bbe0c0df963922caa29b24c9
Shikher Jain <shikherjain786@gmail.com>
2024-09-18 01:07:24 +0530

    numpy

---
+++ ALL IN ONE/03NUMPY/started .py
@@ -0,0 +1,22 @@
+import numpy as np
+
+l=[2,3,4,5]
+print(l)
+print(type(l))
+print()
+arr=np.array(l)
+print(arr)
+print(type(arr))
+
+
+
+a=np.array([1,2,3,4])
+b=np.array([[1,2,3,4],[5,6,7,8]])
+print(a.ndim)
+print(b.ndim)
+
+a=8
+np.ndarray.ndim([1,2,3,4])
+b=np.array([[1,2,3,4],[5,6,7,8]])
+print(a)
+print(b.ndim)
