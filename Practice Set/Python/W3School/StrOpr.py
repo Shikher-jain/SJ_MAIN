b4261e847e92fe92d0f3c15763786dfb6b80f9e4
Shikher Jain <shikherjain786@gmail.com>
2024-09-18 01:08:23 +0530

    W3School

---
+++ Practice Set/Python/W3School/StrOpr.py
@@ -0,0 +1,18 @@
+print(f'The price is {2 + 3} dollars')
+txt = "Hello\rWorld!"
+print(txt) 
+txt = "Hello\nWorld!"
+print(txt) 
+txt = "Hello\tWorld!"
+print(txt) 
+txt = "Hello\bWorld!"
+print(txt) 
+txt = "Hello \f World!"
+print(txt) 
+txt = "Hello\000World!"
+print(txt) 
+txt = "Hello\111World!"
+print(txt) 
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
+
