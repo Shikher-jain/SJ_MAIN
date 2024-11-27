bf0e6994c81ceda3cd3079f6541ab0d5c50cd09d
Shikher Jain <shikherjain786@gmail.com>
2024-11-27 13:54:10 +0530

    MODULE ALPHABETS

---
+++ PYTHON 2.O/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+def K(rows):
+#     for i in range(rows):
+#         ...
+#         # if i ==0 or i==rows-1:
+#         # print("*"*(rows-i))
+#         print("*")
+#         if i==rows-2:
+#             print("^")
