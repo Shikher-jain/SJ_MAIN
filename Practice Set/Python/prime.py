187def5a87643e7c1571a6bbe13b98bc481db2dc
Shikher Jain <shikherjain786@gmail.com>
2024-12-03 22:48:18 +0530

    practice set

--- Practice Set/Python/prime.py
+++ Practice Set/Python/prime.py
@@ -1,14 +1,27 @@
 def is_prime(n):
-    if n in [2, 3]:
-        return True
-    if (n == 1) or (n % 2 == 0):
-        return False
-    r = 3
-    while r * r <= n:
-        if n % r == 0:
-            return False
-        r += 2
-    return True
-print(is_prime(78), is_prime(79))
- 
-5
+    if n <= 1:
+        # return False  # 0 and 1 are not prime numbers
+        return 0
+    # if n == 2 or n == 3:
+    if n in [2,3]:
+        # return True  # 2 and 3 are prime numbers
+        return 1    
+    if n % 2 == 0 or n % 3 == 0:
+        # return False  # Eliminate multiples of 2 and 3
+        return 0
+    
+    # Check divisors from 5 to √n, skipping multiples of 2 and 3
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            # return False
+            return 0
+        i += 6
+    # return True
+    return 1
+
+
+print(is_prime(2))    
+print(is_prime(11))   
+print(is_prime(25))   
+print(is_prime(1))    
